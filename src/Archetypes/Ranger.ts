18454f4b15339fee5b597c5413efe4df4bf46833
Tamiris Shigaki <shigakiendo@gmail.com>
2022-08-09 18:31:40 -0300

    feat: :sparkles: classe Ranger, criada

---
+++ src/Archetypes/Ranger.ts
@@ -0,0 +1,24 @@
+import Archetype from './Archetype';
+import { EnergyType } from '../Energy';
+
+class Ranger extends Archetype {
+  private _energyType: EnergyType;
+  private static _archetypeInstances = 0;
+
+  constructor(name: string) {
+    super(name);
+
+    this._energyType = 'stamina';
+    Ranger._archetypeInstances += 1;
+  }
+
+  public get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  static createdArchetypeInstances(): number {
+    return Ranger._archetypeInstances;
+  }
+}
+
+export default Ranger;
