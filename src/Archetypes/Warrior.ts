5910a9a5ee3a1fee5c4069cfb778f5ca47aa7d4d
Tamiris Shigaki <shigakiendo@gmail.com>
2022-08-09 18:29:08 -0300

    feat: :sparkles: classe Warrior, criada

---
+++ src/Archetypes/Warrior.ts
@@ -0,0 +1,24 @@
+import Archetype from './Archetype';
+import { EnergyType } from '../Energy';
+
+class Warrior extends Archetype {
+  private _energyType: EnergyType;
+  private static _archetypeInstances = 0;
+
+  constructor(name: string) {
+    super(name);
+
+    this._energyType = 'stamina';
+    Warrior._archetypeInstances += 1;
+  }
+
+  public get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  static createdArchetypeInstances(): number {
+    return Warrior._archetypeInstances;
+  }
+}
+
+export default Warrior;
